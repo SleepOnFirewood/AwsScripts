7c64c54bb40fa2600b8390f83b77880000ed05a0
SleepOnFirewood <50755859+SleepOnFirewood@users.noreply.github.com>
2023-08-06 14:09:28 -0400

    Update loadbalancerSGRules.js
    Add promise chain to save SG groups into variable and pass it to SG Detail function so the data can be written to a file.

--- LoadBalancer/loadbalancerSGRules.js
+++ LoadBalancer/loadbalancerSGRules.js
@@ -1,71 +1,82 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
 
-
-// snippet-start:[s3.JavaScript.buckets.listBucketsV3]
-import { DescribeSecurityGroupRulesCommand, DescribeSecurityGroupsCommand, EC2Client } from "@aws-sdk/client-ec2";
+import { DescribeSecurityGroupRulesCommand, EC2Client } from "@aws-sdk/client-ec2";
 import { DescribeLoadBalancersCommand, ElasticLoadBalancingV2Client } from "@aws-sdk/client-elastic-load-balancing-v2";
 import * as fs from 'fs';
+// const rs = require('fs')
+//const rsPromises = require('fs').promises
 
 
-const client = new ElasticLoadBalancingV2Client({region: "us-east-1"});
-const client2 = new EC2Client({region: "us-east-1"})
+const loadBalancerClient = new ElasticLoadBalancingV2Client({region: "us-east-1"});
+const securityGroupClient = new EC2Client({region: "us-east-1"})
 
 
 
- const main = async () => {
+ const getAllLoadBalancers = async () => {
   const command = new DescribeLoadBalancersCommand([]) ;
 
   try {
-    const loadbalancers = await client.send(command);
+    const loadbalancers = await loadBalancerClient.send(command);
     let x = loadbalancers['LoadBalancers'];
-   // console.log(x['LoadBalancers'].length);
+    let loadBalancerTrack = {};
    for(let i in x)
    {
+    loadBalancerTrack[x[i]['LoadBalancerArn']] = x[i]['SecurityGroups']
+
     if(x[i]['SecurityGroups'])
     {
-        console.log(x[i]['SecurityGroups']);
         fs.appendFile('Output.txt', String(x[i]['SecurityGroups'] + ','), err => {
             if (err) throw err;
         })
     }
    }
-  } catch (err) {
+   //read in hashmap into file.
+   for(let lb in loadBalancerTrack)
+   {
+    fs.appendFile('loadBalancerTracker.txt', String(lb + " : " + loadBalancerTrack[lb] + '\n'), err => {
+      if(err) throw err;
+    })
+   }
+  }
+  catch (err) {
     console.error(err);
+  }
+  finally {
+    const data = await fs.promises.readFile('./Output.txt', 'utf8');
+    return(data);
   }
 };
 
-function readFile(fileName)
-{
-  const data = fs.readFileSync(fileName,
-    { encoding: 'utf8', flag: 'r' });
+// function readLoadBalancerFile(fileName) //dont need this anymore.
+// {
+//   const data = fs.readFileSync(fileName,
+//     { encoding: 'utf8', flag: 'r' });
 
-    return data;
-}
+//     return data;
+// }
 
 
-const getSg = async (sgGroups) =>{
+
+
+const getSg = async (loadBalancerSgIds) =>{
   const command = new DescribeSecurityGroupRulesCommand({
     Filters: [
       {
         Name: "group-id",
-        Values: sgGroups
+        Values: loadBalancerSgIds
       },
     ],
   });
 
   try {
-    const securityGroup = await client2.send(command);
-    // for(let i in securityGroup['SecurityGroups'])
-    // {
-    //   if(securityGroup['SecurityGroups'][i]['GroupId'] === 'sg-084b08da89079c06d')
-    //   {
-    //     console.log(securityGroup['SecurityGroups'][i])
-    //   }
-    // }
-    console.log(securityGroup)
+    const securityGroup = await securityGroupClient.send(command);
+
+    for(let i = 0; i<securityGroup['SecurityGroupRules'].length; i++)
+    {
+          fs.appendFile('SecurityGroups.txt', (JSON.stringify(securityGroup['SecurityGroupRules'][i])) + '\n', err => {
+            if(err) throw err;
+            })
+    }
+    
   }
   catch (err)
   {
@@ -75,11 +86,28 @@ const getSg = async (sgGroups) =>{
 
 
 
-//main();
+// async function resolve()
+// {
+//   await getAllLoadBalancers();
+//   let loadBalancerSgIds = readLoadBalancerFile('Output.txt')
+//   loadBalancerSgIds = loadBalancerSgIds.split(',')
+//   loadBalancerSgIds.pop(); //remove last entry as as sult of separation via ','
+//   getSg(loadBalancerSgIds)
+// }
 
 
-let x = readFile('./Output.txt')
-x = x.split(',')
-x.pop();
+let getLb = getAllLoadBalancers();
+getLb.then((data) =>{
+  let loadBalancerSgIds = data.split(',')
+  loadBalancerSgIds.pop(); //remove last entry as as sult of separation via ','
+  getSg(loadBalancerSgIds)
+})
 
-getSg(x)
+
+
+
+// let loadBalancerSgIds = readLoadBalancerFile('Output.txt')
+
+
+
+//resolve();
